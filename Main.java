b696d6d4dc7ae533bede1233c9199648eb1af4af
Krishna Jadhav <krishanraviandrajadhav@gmail.com>
2026-04-11 01:57:26 +0530

    Day 3

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello from Jenkins + GitHub");
-        System.out.println("Auto trigger  (GitHub hook trigger for GITScm polling)");
-        System.out.println("Auto trigger  (GitHub hook trigger for GITScm polling) check 3");
-    }
-}
